04a7d3955f73b1a12e10cf40657eec40a0638332
Hy-gon <mickael.lemieux@outlook.com>
2020-12-01 15:49:32 +0100

    No template NAME ROUTE

--- TP1 FLask/app.py
+++ TP1 FLask/app.py
@@ -11,5 +11,12 @@ app = Flask(__name__)
 def index():
     return "Hello H3"
 
+# Without Index Template
+
+@app.route("/routeName/<name>", methods=["GET"])
+def get_name(name):
+    return "Hello " + name
+
+
 if __name__ == "__main__":
     app.run(debug=True)
